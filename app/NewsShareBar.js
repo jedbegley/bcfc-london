2ed492b1388c3faf7360d9cf20663934c5583f1f
jedbegley <jed_begley@hotmail.co.uk>
2026-09-03 17:23:59 +0100

    Create NewsShareBar.js

---
+++ app/NewsShareBar.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { createPortal } from "react-dom";
+import ShareButtons from "./ShareButtons";
+
+export default function NewsShareBar() {
+  const pathname = usePathname();
+  const [mountPoint, setMountPoint] = useState(null);
+
+  useEffect(() => {
+    if (pathname === "/news") {
+      setMountPoint(null);
+      return;
+    }
+
+    const footer = document.querySelector("footer");
+
+    if (!footer) return;
+
+    const shareContainer = document.createElement("div");
+
+    shareContainer.style.maxWidth = "1080px";
+    shareContainer.style.margin = "0 auto";
+    shareContainer.style.padding = "0 40px 40px";
+
+    footer.parentNode.insertBefore(shareContainer, footer);
+
+    setMountPoint(shareContainer);
+
+    return () => {
+      shareContainer.remove();
+    };
+  }, [pathname]);
+
+  if (!mountPoint) return null;
+
+  return createPortal(
+    <ShareButtons title="Bristol City London Supporters FC" />,
+    mountPoint
+  );
+}
