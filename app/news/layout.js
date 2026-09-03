a01cb0199a7463f68c240657483b65215375333a
jedbegley <jed_begley@hotmail.co.uk>
2026-09-03 17:22:18 +0100

    Update layout.js

--- app/news/layout.js
+++ app/news/layout.js
@@ -1,27 +1,3 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-import ShareButtons from "../ShareButtons";
-
 export default function NewsLayout({ children }) {
-  const pathname = usePathname();
-  const isArticle = pathname !== "/news";
-
-  return (
-    <>
-      {children}
-
-      {isArticle && (
-        <div
-          style={{
-            maxWidth: "900px",
-            margin: "0 auto 40px",
-            padding: "0 20px",
-          }}
-        >
-          <ShareButtons />
-        </div>
-      )}
-    </>
-  );
+  return children;
 }
