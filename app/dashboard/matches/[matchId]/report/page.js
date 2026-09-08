0deab45fb4feec0221079629359751f3c1f38848
jedbegley <jed_begley@hotmail.co.uk>
2026-09-08 19:55:40 +0100

    Create page.js

---
+++ app/dashboard/matches/[matchId]/report/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function MatchReportPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Manager Match Report</h1>
+      <p>Match report form coming next.</p>
+    </main>
+  );
+}
