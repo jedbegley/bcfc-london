17c8b555dbcfc712794653d1df966fae86679ce9
jedbegley <jed_begley@hotmail.co.uk>
2026-09-08 18:24:56 +0100

    Update page.js

--- app/squad/page.js
+++ app/squad/page.js
@@ -14,7 +14,7 @@ const [loading, setLoading] = useState(true);
   async function loadPlayers() {
     const { data, error } = await supabase
      .from("public_squad")
-.select("id, full_name, squad_number, position, public_role, squad_group")
+.select("id, full_name, squad_number, position, public_role, squad_group, photo_url")
 .order("squad_number", { ascending: true });
 
     if (error) {
