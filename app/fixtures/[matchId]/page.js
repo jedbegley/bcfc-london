9c695f9ea573093db96c442b55661e1e433aaf33
jedbegley <jed_begley@hotmail.co.uk>
2026-09-13 17:39:43 +0100

    Update page.js

--- app/fixtures/[matchId]/page.js
+++ app/fixtures/[matchId]/page.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import Link from "next/link";
 import MotmVote from "./MotmVote";
-
+// Match report page
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
