43831948bca21405d3f255e8fbb91a5a39ddb29e
jedbegley <jed_begley@hotmail.co.uk>
2026-09-13 14:16:52 +0100

    Update page.js

--- app/stats/page.js
+++ app/stats/page.js
@@ -4,6 +4,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
+export const dynamic = "force-dynamic";
 export default async function StatsPage() {
     const { data: stats, error } = await supabase
     .from("match_stats")
